48ef45f667775671727987d2c42974e11a9dd855
lawanu <lawanu1@gmail.com>
2023-11-08 17:11:09 +0530

    added all the files of crm-fe

---
+++ src/api/tickets.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+
+const BASE_URL = "https://crm-backend2.onrender.com"
+
+export async function fetchTicket() {
+    return await axios.get(`${BASE_URL}/crm/api/v1/tickets/`, {
+        headers: {
+            'x-access-token': localStorage.getItem("token")
+        }
+    }, {
+        "userId": localStorage.getItem("userId")
+    }
+    )
+
+}
+
+// POST API : 
+// asking for data in params : title, description
+export async function ticketCreation(data) {
+    return await axios.post(`${BASE_URL}/crm/api/v1/tickets/`, data, {
+        headers: {
+            'x-access-token': localStorage.getItem("token")
+        }
+    })
+}
+
+
+
+// PUT API : passing the id of the ticket and the new updated data 
+
+export async function ticketUpdation(id, selectedCurrTicket) {
+    return await axios.put(`${BASE_URL}/crm/api/v1/tickets/${id}`, selectedCurrTicket, {
+        headers: {
+            'x-access-token': localStorage.getItem("token")
+        }
+    }, {
+        "userId": localStorage.getItem("userId")
+    })
+}
